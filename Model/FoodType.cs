41e075afe5071aea97660597f513d09e37310eea
byadav1 <byadav1@my.westga.edu>
2022-06-09 22:04:27 -0400

    adding model file

--- Model/FoodType.cs
+++ Model/FoodType.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipeBookApp.Model
+﻿namespace RecipeBookApp.Model
 {
-    class FoodType
+    public class FoodType
     {
     }
 }
