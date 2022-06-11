15ceac377e0d5a357c3c071707d8afb8133012e7
byadav1 <byadav1@my.westga.edu>
2022-06-10 23:08:21 -0400

    Commting the changes for recipe - add,delete,update recipe

--- Controller/RecipeController.cs
+++ Controller/RecipeController.cs
@@ -1,6 +1,5 @@
 ﻿
 using System.Collections.Generic;
-
 using RecipeBookApp.DAL;
 using RecipeBookApp.Model;
 
@@ -17,6 +16,36 @@ namespace RecipeBookApp.Controller
             return RecipeDAL.GetRecipes();
         }
 
-       
+
+        /// <summary>
+        /// Updates the recipe.
+        /// </summary>
+        /// <param name="newUpdateRecipe">The new update recipe.</param>
+        /// <param name="oldUpdateRecipe">The old update recipe.</param>
+        /// <returns></returns>
+        public List<Recipe> UpdateRecipe(List<Recipe> newUpdateRecipe, List<Recipe> oldUpdateRecipe)
+        {
+            return RecipeDAL.UpdateRecipe(newUpdateRecipe, oldUpdateRecipe);
+        }
+
+        /// <summary>
+        /// Adds the recipe.
+        /// </summary>
+        /// <param name="newRecipe">The new recipe.</param>
+        public void AddRecipe(List<Recipe> newRecipe)
+        {
+             RecipeDAL.AddRecipe(newRecipe);
+        }
+
+
+        /// <summary>
+        /// Deletes the recipes.
+        /// </summary>
+        /// <param name="deleteRecipeId">The delete recipe identifier.</param>
+        /// <returns></returns>
+        public List<Recipe> DeleteRecipes(int deleteRecipeId)
+        {
+            return RecipeDAL.DeleteRecipe(deleteRecipeId);
+        }
     }
 }
