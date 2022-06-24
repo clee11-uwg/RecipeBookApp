05faa72489d7c1506c1910515db3ff8f4c8029a8
Greg Murrell <j_greg_m@comcast.net>
2022-06-24 19:35:34 -0400

    up to date with queries

--- Controller/IngredientsController.cs
+++ Controller/IngredientsController.cs
@@ -16,43 +16,34 @@ namespace RecipeBookApp.Controller
 
         private readonly IngredientsDAL ingredientsDAL;
 
-
+        /// <summary>
+        /// Instantiaties instance variables
+        /// </summary>
         public IngredientsController()
         {
             this.ingredientsDAL = new IngredientsDAL();
         }
 
         /// <summary>
-        /// Gets all Ingredient from the database table.
+        /// Gets all Ingredients from the database table.
         /// </summary>
-        /// <returns>List of all Recipes from database </returns>
+        /// <returns>List of all Ingredients from database </returns>
         public List<Ingredient> GetIngredients()
         {
-            List<Ingredient> ingredientsList = this.ingredientsDAL.GetIngredients();
-
-            if (!ingredientsList.Any())
-            {
-                throw new ArgumentNullException("No Ingredients found on the database");
-
-            }
-            return ingredientsList;
+            return this.ingredientsDAL.GetIngredients();
         }
 
         /// <summary>
         /// Gets the Ingredient for the search ID.
         /// </summary>
         /// <returns>Ingredient found in the database</returns>
-        public Ingredient GetIngredient(int searchRecipeID)
+        public List<Ingredient> GetIngredient(int searchRecipeID)
         {
-            Ingredient foundIngredient = this.ingredientsDAL.GetIngredient(searchRecipeID);
-            if (foundIngredient == null)
+            if (searchRecipeID < 1)
             {
-                throw new ArgumentNullException("No Ingredient found on the database");
-
+                throw new ArgumentOutOfRangeException("Recipe ID cannot be less than 1");
             }
-            return foundIngredient;
+            reutrn this.ingredientsDAL.GetIngredient(searchRecipeID);
         }
-
-
     }
 }
