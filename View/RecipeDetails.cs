d19e8503e98f1c452b50c9195f08546372e41b02
leecodya <lee.codya@gmail.com>
2022-06-25 15:56:00 -0400

    added ingredient list view to recipe details page and added a method that would pull in the ingredients and populate the ingredients list view with the ingredients for the selected recipe

--- View/RecipeDetails.cs
+++ View/RecipeDetails.cs
@@ -16,18 +16,39 @@ namespace RecipeBookApp.View
     {
         private Recipe selectedRecipe;
         private RecipeController recipeController;
+        private IngredientsController ingredientsController;
 
+        /// <summary>
+        /// 0 param constructor
+        /// </summary>
         public RecipeDetails()
         {
             InitializeComponent();
             this.selectedRecipe = new Recipe();
             this.recipeController = new RecipeController();
+            this.ingredientsController = new IngredientsController();
         }
 
+        /// <summary>
+        /// Method to set the Recipe to the selected recipe
+        /// </summary>
+        /// <param name="userSelectedRecipe">Selected recipe from user</param>
         public void SetRecipe(Recipe userSelectedRecipe)
         {
             this.selectedRecipe = userSelectedRecipe;
             this.titleLbl.Text = this.selectedRecipe.RecipeName;
+            GetRecipeDetails(this.selectedRecipe);
+        }
+
+        private void GetRecipeDetails(Recipe selectedRecipe)
+        {
+            List<Ingredient> ingredientList = this.ingredientsController.GetIngredient(selectedRecipe.RecipeId);
+            for (int i = 0; i < ingredientList.Count; i++)
+            {
+                string ingredient = ingredientList[i].IngredientName;
+                var listViewItem = new ListViewItem(ingredient);
+                ingredientsListView.Items.Add(listViewItem);
+            }
         }
     }
 }
