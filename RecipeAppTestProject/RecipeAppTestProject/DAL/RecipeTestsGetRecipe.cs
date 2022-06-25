9274e768f818ee43f4021404fdd95629fb72d21d
yadav <yadavbg@gmail.com>
2022-06-25 17:27:45 -0400

    adding test folder

---
+++ RecipeAppTestProject/RecipeAppTestProject/DAL/RecipeTestsGetRecipe.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeBookApp.DAL;
+using RecipeBookApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+
+namespace RecipeAppTestProject
+{
+    [TestClass]
+    public class RecipeTestsGetRecipetestRecipesList
+    {
+        //  private const string Expected = "Hello World!";
+       
+        [TestMethod]
+        public void TestGetRecipesMethod()
+        {
+            List<Recipe> recipetestList  = new List<Recipe>();
+            RecipeDAL testRecipeDal = new RecipeDAL();
+            recipetestList = testRecipeDal.GetRecipes();
+
+          
+            Assert.AreEqual(2, recipetestList.Count);
+        }
+
+    
+    }
+}
