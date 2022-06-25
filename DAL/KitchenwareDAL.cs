0636a1f534b0838452e82bb606caac6209eddce9
Greg Murrell <j_greg_m@comcast.net>
2022-06-25 15:31:56 -0400

    clean up "GetKichenware(recipeID)"

--- DAL/KitchenwareDAL.cs
+++ DAL/KitchenwareDAL.cs
@@ -7,7 +7,7 @@ namespace RecipeBookApp.DAL
 {
     /// <summary>
     /// This class serves as the Data Access Layer
-    /// for the  DB Recipe table.
+    /// for the  DB Kitchenware table.
     /// </summary>
     public class KitchenwareDAL
     {
@@ -15,7 +15,7 @@ namespace RecipeBookApp.DAL
         /// <summary>
         /// This method to get the list of Kitchenware avilable in the recipe database
         /// </summary>
-        /// <returns></returns>
+        /// <returns>List of all kitchenware known to the database</returns>
         public List<Kitchenware> GetKitchenwares()
         {
             List<Kitchenware> cookwareList = new List<Kitchenware>();
@@ -45,34 +45,39 @@ namespace RecipeBookApp.DAL
         }
 
         /// <summary>
-        /// This method to get the recipe based on the  search ID
-        /// </summary>
-        /// <returns></returns>
-        public Kitchenware GetKitchenware(int recipeID)
+        /// Finds all kitchenware needed to cook said recipe
+        /// <param name="recipeID">Id of recipe</param>
+        /// <returns>Kitchenware associated with recipe</returns>
+        public List<Kitchenware> GetKitchenware(int recipeID)
         {
-            Kitchenware kitchen = null;
+            List<Kitchenware> cookwareList = new List<Kitchenware>();
             string selectStatement = "SELECT kitchenware.id, kitchenware.kitchenware " +
                  "FROM recipe JOIN recipe_uses_kitchenware ON recipe.id = recipe_uses_kitchenware.recipeID " +
                      "JOIN kitchenware ON kitchenware.id = recipe_uses_kitchenware.kitchenwareID " +
-                     "WHERE recipe.id = recipeID;";
+                     "WHERE recipe.id = @recipeID;";
 
             using (SQLiteConnection connection = DBConnection.GetConnection())
             {
                 using (SQLiteCommand selectCommand = new SQLiteCommand(selectStatement, connection))
                 {
+                    selectCommand.Parameters.AddWithValue("@recipeID", recipeID);
                     using (SQLiteDataReader reader = selectCommand.ExecuteReader())
                     {
 
-                        kitchen = new Kitchenware
+                        while (reader.Read())
                         {
-                            KitchenwareId = Convert.ToInt32(reader["id"]),
-                            KitchenwareDetails = reader["kitchenware"].ToString()
-                        };
+                            Kitchenware kitchen = new Kitchenware
+                            {
+                                KitchenwareId = Convert.ToInt32(reader["id"]),
+                                KitchenwareDetails = reader["Kitchenware"].ToString(),
+                            };
 
-                    }
+                            cookwareList.Add(kitchen);
+
+                        }
                 }
             }
-            return kitchen;
+            return cookwareList;
         }
     }
 }
