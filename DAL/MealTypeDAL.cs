61e06ccefd1d457bf0c8fe3a6488d9a67434943d
Greg Murrell <j_greg_m@comcast.net>
2022-06-27 22:38:56 -0400

    GetMealTypes(int)

--- DAL/MealTypeDAL.cs
+++ DAL/MealTypeDAL.cs
@@ -44,5 +44,44 @@ namespace RecipeBookApp.DAL
             return mealList;
         }
 
+
+        /// <summary>
+        /// This method to get the list of all mealtype known to the recipe database
+        /// </summary>
+        /// <param name="recipeID">Id of recipe</param>
+        /// <returns>List of Allergens known to the recipe database</returns>
+        public List<MealType> GetMealTypes(int recipeID)
+        {
+            List<MealType> mealList = new List<MealType>();
+            string selectStatement = @"SELECT type_of_meal.id, type_of_meal.type 
+                                        FROM type_of_meal
+                                            JOIN recipe_is_a_type_of_meal ON type_of_meal.id = recipe_is_a_type_of_meal.typeOfMealID
+                                            JOIN recipe ON recipe.id = recipe_is_a_type_of_meal.recipeID
+                                        WHERE recipe.id = @recipeID; ";
+
+            using (SQLiteConnection connection = DBConnection.GetConnection())
+            {
+                using (SQLiteCommand selectCommand = new SQLiteCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@recipeID", recipeID);
+                    using (SQLiteDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MealType meal = new MealType
+                            {
+                                mealTypeID = Convert.ToInt32(reader["id"]),
+                                type = reader["type"].ToString()
+                            };
+
+                            mealList.Add(meal);
+                        }
+                    }
+                }
+            }
+
+            return mealList;
+        }
+
     }
 }
