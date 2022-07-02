c89be7e27ba4d111527ce83ac01a9ae4b3f88cfc
Greg Murrell <j_greg_m@comcast.net>
2022-07-02 14:58:28 -0400

    controller exception tests all pass

---
+++ RecipeAppTestProject/RecipeAppTestProject/Controller/TestIngredientsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeBookApp.Controller;
+using RecipeBookApp.Model;
+
+namespace RecipeAppTestProject.Controller
+{
+    /// <summary>
+    /// Class containing test methods for Ingredients Controller
+    /// </summary>
+    [TestClass]
+    public class TestIngredientsController
+    {
+        IngredientsController controller;
+
+        /// <summary>
+        /// Initialize IngredientsController used in tests
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            controller = new IngredientsController();
+        }
+
+        /// <summary>
+        /// Tests that GetIngredients(int) throws errors if int is 0
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGetIngredientsByRecipeIDThrowsExceptionIfZero()
+        {
+            List<Ingredient> ingredients = controller.GetIngredient(0);
+        }
+
+        /// <summary>
+        /// Tests that GetIngredients(int) throws errors if int is negative
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGetIngredientsByRecipeIDThrowsExceptionIfNegative()
+        {
+            List<Ingredient> ingredients = controller.GetIngredient(-1);
+        }
+    }
+}
---
+++ RecipeAppTestProject/RecipeAppTestProject/Controller/TestNutritionController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecipeBookApp.Controller;
+using RecipeBookApp.Model;
+
+
+namespace RecipeAppTestProject.Controller
+{
+    /// <summary>
+    /// Class containing test methods for Nutrition Controller
+    /// </summary>
+    [TestClass]
+    public class TestNutritionController
+    {
+        NutritionController controller;
+
+        /// <summary>
+        /// Initialize NutritionController used in tests
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
+        {
+            controller = new NutritionController();
+        }
+
+        /// <summary>
+        /// Tests that GetNutrition(int) throws errors if int is 0
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGetNutritionByRecipeIDThrowsExceptionIfZero()
+        {
+            List<Nutrition> nutritions = controller.GetNutrition(0);
+        }
+
+        /// <summary>
+        /// Tests that GetNutrition(int) throws errors if int is negative
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGetNutritionByRecipeIDThrowsExceptionIfNegative()
+        {
+            List<Nutrition> nutritions = controller.GetNutrition(-1);
+        }
+    }
+}
