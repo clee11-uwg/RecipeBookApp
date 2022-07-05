a960452b3274e1cb39efa9dc347d099d74d8c8ae
yadav <yadavbg@gmail.com>
2022-07-04 22:29:44 -0400

    Commiting the chnages to included controller folder on test projects

---
+++ RecipeAppTestProject/RecipeAppTestProject/Model/ImageTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RecipeAppTestProject.Model
+{
+    [TestClass]
+    public class ImageTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+
+        }
+    }
+}
--- RecipeAppTestProject/RecipeAppTestProject/Model/MealTypeTest.cs
+++ RecipeAppTestProject/RecipeAppTestProject/Model/MealTypeTest.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RecipeBookApp.Model;
-using System;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
 
 namespace RecipeAppTestProject.Model
 {
