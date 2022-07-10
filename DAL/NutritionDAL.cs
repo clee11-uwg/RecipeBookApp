6984725b8384abafe8f2fbc07ec7321943fb2abe
Greg Murrell <j_greg_m@comcast.net>
2022-07-10 19:35:50 -0400

    add/delete nutrition

--- DAL/NutritionDAL.cs
+++ DAL/NutritionDAL.cs
@@ -96,6 +96,75 @@ namespace RecipeBookApp.DAL
             }
             return nutritionList;
         }
+
+        /// <summary>
+        /// Adds the Nutrition to the database
+        /// </summary>
+        /// <param name="nutrition">Nutrition to add</param>
+        /// <returns>Whether or not the addition was successful</returns>
+        public bool AddNutrition(Nutrition nutrition)
+        {
+            int result = -1;
+            string selectStatement = @"INSERT INTO nutrition (carbohydrate, fat, protein, alcohol, calories, serving_size)
+                                    VALUES (@carbohydrate, @fat, @protein, @alcohol, @calories, @serving_size);";
+
+            using (SQLiteConnection connection = DBConnection.GetConnection())
+            {
+                using (SQLiteCommand selectCommand = new SQLiteCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@carbohydrate", nutrition.Carbohydrate);
+                    selectCommand.Parameters.AddWithValue("@fat", nutrition.Fat);
+                    selectCommand.Parameters.AddWithValue("@protein", nutrition.Protein);
+                    selectCommand.Parameters.AddWithValue("@alcohol", nutrition.Alcohol);
+                    selectCommand.Parameters.AddWithValue("@calories", nutrition.Calories);
+                    selectCommand.Parameters.AddWithValue("@serving_size", nutrition.ServingSize);
+
+                    connection.Open();
+                    result = selectCommand.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            if (result < 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Deletes the Nutrition to the database
+        /// </summary>
+        /// <param name="nutrition">Nutrition to delete</param>
+        /// <returns>Whether or not the deletion was successful</returns>
+        public bool DeleteNutrition(Nutrition nutrition)
+        {
+            int result = -1;
+            string selectStatement = @"DELETE FROM nutrition
+                                    WHERE id = @id;";
+
+            using (SQLiteConnection connection = DBConnection.GetConnection())
+            {
+                using (SQLiteCommand selectCommand = new SQLiteCommand(selectStatement, connection))
+                {
+                    selectCommand.Parameters.AddWithValue("@id", nutrition.NutritionId);
+
+                    connection.Open();
+                    result = selectCommand.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            if (result < 1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
     }
 
 }
