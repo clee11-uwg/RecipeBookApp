6d300d4ae1bbe4d4cf4f510f2e98bfbca0f454ca
Greg Murrell <j_greg_m@comcast.net>
2022-07-17 20:15:47 -0400

    cleaned up

--- RecipeAppTestProject/RecipeAppTestProject/Controller/TestEthnicOriginController.cs
+++ RecipeAppTestProject/RecipeAppTestProject/Controller/TestEthnicOriginController.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RecipeBookApp.Controller;
-using RecipeBookApp.Model;
 
 namespace RecipeAppTestProject.Controller
 {
