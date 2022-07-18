98c4ad66b1d020abc6eab68be99b7a4d4d9521e5
Greg Murrell <j_greg_m@comcast.net>
2022-07-17 20:09:15 -0400

    remove unnecessary imports

--- Controller/EthnicOriginController.cs
+++ Controller/EthnicOriginController.cs
@@ -2,7 +2,6 @@ using RecipeBookApp.DAL;
 using RecipeBookApp.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace RecipeBookApp.Controller
 {
--- Controller/NutritionController.cs
+++ Controller/NutritionController.cs
@@ -2,7 +2,6 @@
 using RecipeBookApp.Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace RecipeBookApp.Controller
 {
